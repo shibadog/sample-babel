dae7d71abffe173599f214664cb3c683bdd57e78
shibadog <shibadog99@gmail.com>
2019-11-11 17:48:54 +0900

    とりあえず

---
+++ src/main/js/index.js
@@ -0,0 +1,9 @@
+import Aaa from "./package/aaaclass.js";
+
+const arr = [1, 2, 3];
+arr.map(n => n ** 2);
+
+console.log(arr);
+
+const aaa = new Aaa("aaaa");
+console.log(aaa.print());
---
+++ src/main/js/package/aaaclass.js
@@ -0,0 +1,11 @@
+class Hello {
+    constructor(value){
+        this.value = value
+    }
+
+    print() {
+        return this.value;
+    }
+}
+
+module.exports = Hello;
